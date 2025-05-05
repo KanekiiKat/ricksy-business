350b5e9837a278ee8ab92f046b609eb2eea9ecff
KanekiiCat <alex123viri@gmail.com>
2025-05-05 14:59:28 +0200

    feat(UfosPark): Añadir funciones.

--- src/main/java/edu/estatuas/functions/UfosPark.java
+++ src/main/java/edu/estatuas/functions/UfosPark.java
@@ -1,11 +1,12 @@
 package edu.estatuas.functions;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class UfosPark {
 
 
-    private Double fee;
+    private Double fee = 500d;
     private HashMap<String, String> flota;
 
 
@@ -13,14 +14,31 @@ public class UfosPark {
 
     public void UfosPark(){}
 
-    public void add(String ufo){
-        flota.putIfAbsent(ufo, null);
+    public void add(String ufoID){
+        flota.putIfAbsent(ufoID, null);
     }
 
     public void dispatch(CreditCard card){
-        
+
+        for (String numberInFlota : flota.values() ){
+            if (card.number() != numberInFlota){
+                if (card.pay(fee)){
+                    for (String ufo : flota.keySet()){
+                        flota.replace(ufo, card.number());
+                    }
+                }
+            }
+        }
     }
 
-    
+    public String getUfoOf(String number){
+        for (String ufoInFlota : flota.keySet() ){
+            if (flota.values().contains(number)){
+                return ufoInFlota;
+            } else {
+                return "Null";
+            }
+        }
+    }
 
 }
